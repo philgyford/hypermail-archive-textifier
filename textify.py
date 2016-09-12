5f6fdb33194526262278f10c4b09763e29b0a588
Phil Gyford <phil@gyford.com>
2016-09-12 20:22:27 +0100

    Use Message-IDs instead of filenames.

--- textify.py
+++ textify.py
@@ -26,6 +26,14 @@ class Textifier(object):
     mailman_archive_url = None
     save_path = None
 
+    # Will map original message directory/filenames like '2013Aug/0002.html' to
+    # Message-IDs.
+    name_to_id = {}
+
+    # For every message that needs an In-Reply-To added to it at the end of
+    # the process, map from Message-ID to '2013Aug/0002.html'
+    replies = {}
+
     def __init__(self, config):
         """
         Set up config variables, create the directory to save messages in.
@@ -69,7 +77,12 @@ class Textifier(object):
             self.scrapeMessage(url)
             time.sleep(0.5) # Play nice, wait half a second.
 
+        self.addReplies()
+
     def scrapeIndex(self, url):
+        """
+        Returns a list of URLs for month archives, in chronological order.
+        """
         source = self.fetchPage(url)
         soup = BeautifulSoup(source, 'html.parser')
 
@@ -82,9 +95,12 @@ class Textifier(object):
             except IndexError:
                 pass # First row only has <th>s.
 
-        return month_urls
+        return list(reversed(month_urls))
 
     def scrapeMonth(self, url):
+        """
+        Returns a list of URLs of individual messages, in chronological order.
+        """
         source = self.fetchPage(url)
         soup = BeautifulSoup(source, 'html.parser')
 
@@ -107,57 +123,120 @@ class Textifier(object):
                 path = message.a.get('href')
                 message_urls.append( urlparse.urljoin(url, path) )
 
-        return message_urls
+        return list(reversed(message_urls))
 
     def scrapeMessage(self, url):
 
+        # Get ['2016Mar', '0002.html'] from 'http://.../2016Mar/0002.html':
         url_parts = urlparse.urlsplit(url).path.split('/')
-        # Get '2016Mar' from 'http://.../2016Mar/0002.html':
         month = url_parts[-2:-1][0]
-        # Get '0002.html' from 'http://.../2016Mar/0002.html':
         filename = url_parts[-1:][0]
-        # Change '0002.html' to '0002.txt':
-        save_filename = '%s.txt' % os.path.splitext(filename)[0]
-        # End up with '2016Mar_0002.html':
-        save_filename = '%s_%s' % (month, save_filename)
+        # For putting the Message Id in self.name_to_id:
+        message_name = '%s/%s' % (month, filename)
 
         source = self.fetchPage(url)
         soup = BeautifulSoup(source, 'html.parser')
 
+        # The message, including headers:
         message = soup.find(class_='mail')
 
         headers = message.find(class_='headers')
+
+        # Get the Message-ID for putting in self.name_to_id:
+        message_id_text = headers.find(id='message-id').get_text()
+        m = re.search('<(.*?)>', message_id_text)
+        message_id = m.group(1)
+        # Store for future reference:
+        self.name_to_id[message_name] = message_id
+
+        # Tidy the header text for saving:
         headers = headers.get_text()
         headers = headers.strip()
         headers = re.sub('\n+', '\n', headers)
 
+        # Get the body of the message:
         body = message.find(id='body')
         body = body.get_text()
+
+
+        # Work out in-reply-to headers:
 
         # Looks for a link like:
         # <map id="navbar">
         #   <ul class="links">
         #       <a href="0002.html">In reply to</a>
-        # And gets the '0002.html', and adds a line like this to headers:
-        # Reply-To: 2016Mar_0002.txt
+        # Uses that to find the Message-ID of the message this is in reply to.
+        # And adds an In-Reply-To line to the headers.
         for a in soup.find(id='navbar').find(class_='links').find_all('a'):
             if a.get_text() == 'In reply to':
-                # e.g. '0002.html':
-                href = a.get('href')
-                # e.g. '2016Mar_0002.txt':
-                reply_to_filename = '%s_%s.txt' % (
-                                            month, os.path.splitext(href)[0])
-                headers = '%s\nReply-To: %s' % (headers, reply_to_filename)
+                reply_to_link = a.get('href')
+
+                # Two things might happen here...
+                #
+                # Either the link is to a Message-ID, in which case we can
+                # add the In-Reply-To header right now.
+                #
+                # Or the link is a filename ('0002.html') in which case,
+                # we need to save the fact this Message-ID is in reply to
+                # that filename.
+
+                if reply_to_link.startswith('http'):
+                    # Probably like 'https://www.w3.org/mid/CAEnTvdCE9nprxRAikQmyAp19XkNZG=z-vUErs8c4Ho_y6DD0GA@mail.gmail.com'
+
+                    reply_to_id = urlparse.urlsplit(reply_to_link).path.split('/')[-1:][0]
+                    headers = 'In-Reply-To: <%s>\n%s' % (reply_to_id, headers)
+
+                else:
+                    # Link is probably like '0002.html'
+                    # Make name like '2016Mar/0002.html':
+                    reply_to_name = '%s/%s' % (month, reply_to_link)
+                    # Store which filename we're replying to, for adding later.
+                    self.replies[message_id] = reply_to_name
+
                 break
+
 
         txt = "%s\n%s" % (headers, body)
 
-        file = codecs.open(
-                        os.path.join(self.save_path, save_filename),
-                        'w',
-                        encoding='utf-8')
-        file.write(txt)
-        file.close()
+        save_filename = '%s.txt' % message_id
+        path = os.path.join(self.save_path, save_filename)
+        with codecs.open(path, 'w', encoding='utf-8') as f:
+            f.write(txt)
+            f.close()
+
+    def addReplies(self):
+        """
+        We will have populated self.replies and self.name_to_id.
+        Use these to work out which messages need 'In-Reply-To' lines
+        adding to them, and do that.
+
+        Why is this done separately?
+        Because if a message just says it's in reply to '0002.html' we can't
+        get the ID of that message at the time. So we go through ALL of the
+        messages, save them, and store the info mapping filename to ID.
+
+        THEN once all messages have been saved, we have enough info to go
+        back and map from these filenames to IDs, and add the header lines.
+        """
+        self.message("Adding In-Reply-To header lines to files")
+
+        # Go through each message that's a reply.
+        # Get its ID and the message_name of what it's replying to:
+        for message_id, message_name in self.replies.iteritems():
+
+            # Get the Message-ID from the name (like '2016/Mar/0002.html'):
+            reply_to_id = self.name_to_id[message_name]
+
+            filename = '%s.txt' % message_id
+            path = os.path.join(self.save_path, filename)
+
+            with codecs.open(path, 'r+', encoding='utf-8') as f:
+                txt = f.read()
+                line = 'In-Reply-To: <%s>' % reply_to_id
+                txt = '%s\n%s' % (line, txt)
+                f.seek(0)
+                f.write(txt)
+                f.truncate()
 
     def fetchPage(self, url):
         "Used for fetching all the remote pages."
